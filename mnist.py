7c9259d4f8b669ed2de837388194762c17fb1a2c
_329_ <329g329@gmail.com>
2016-06-26 01:19:37 +0900

    add mnist_ans

--- mnist.py
+++ mnist.py
@@ -14,42 +14,6 @@ mnist.target = mnist.target.astype(np.int32)
 train_data, test_data, train_label, test_label = train_test_split(mnist.data, mnist.target, test_size=10000,
                                                                   random_state=222)
 
+print "data shape ", mnist.data.dtype, mnist.data.shape
+print "label shape ", mnist.target.dtype, mnist.target.shape
 
-class MnistModel(chainer.Chain):
-    def __init__(self):
-
-        super(MnistModel, self).__init__(
-                l1=L.Linear(784, 100),
-                l2=L.Linear(100, 100),
-                l3=L.Linear(100, 10),
-        )
-
-    def __call__(self, x, t, train=True):
-
-        x = chainer.Variable(x)
-        t = chainer.Variable(t)
-
-        h = F.relu(self.l1(x))
-        h = F.relu(self.l2(h))
-        h = self.l3(h)
-
-        if train:
-            return F.softmax_cross_entropy(h, t), F.accuracy(h, t)
-        else:
-            return F.accuracy(h, t)
-
-
-model = MnistModel()
-optimizer = optimizers.Adam()
-optimizer.setup(model)
-
-for epoch in range(100):
-    model.zerograds()
-    loss, acc = model(train_data, train_label)
-    loss.backward()
-    print "epoch %3d : %.4f" % (epoch, acc.data)
-    optimizer.update()
-
-model.zerograds()
-acc = model(test_data, test_label, train=False)
-print "test acc : ", acc.data
